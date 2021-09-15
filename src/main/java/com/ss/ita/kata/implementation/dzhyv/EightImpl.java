c000b247900e7cacdf2249750608f9af8f1eafee
Dmytro Zhyvko <d.zhyvko@gmail.com>
2021-09-15 16:49:31 +0300

    Implemented To square(root) or not to square(root) method

--- src/main/java/com/ss/ita/kata/implementation/dzhyv/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/dzhyv/EightImpl.java
@@ -20,7 +20,15 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+    	int[] finalArray = new int[array.length];
+        for (int i = 0; i < array.length; i++){
+        	if (Math.sqrt(array[i])%1 == 0){
+        		finalArray[i] = (int)Math.sqrt(array[i]);
+        		} else {
+        			finalArray[i] = array[i]*array[i];        		
+        		}
+        }
+    	return finalArray;
     }
 
     @Override
