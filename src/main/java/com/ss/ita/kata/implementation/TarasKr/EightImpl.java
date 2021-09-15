85c742e8d6b10306c2d219c43bb8b3f51df183ff
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-09-15 13:54:01 +0300

    edit

--- src/main/java/com/ss/ita/kata/implementation/TarasKr/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/TarasKr/EightImpl.java
@@ -3,11 +3,25 @@ package com.ss.ita.kata.implementation.TarasKr;
 import com.ss.ita.kata.Eight;
 
 public class EightImpl implements Eight {
+ 
+	/**
+	 * Nathan loves cycling. Because Nathan knows it is important to stay hydrated,
+	 * he drinks 0.5 litres of water per hour of cycling. You get given the time in
+	 * hours and you need to return the number of litres Nathan will drink, rounded
+	 * to the smallest value.
+	 * 
+	 * @param time - A time
+	 * @exception IllegalArgumentException IF (time < 0)
+	 * @return the number of litres rounded to the smallest value.
+	 */
+	@Override
+	public int liters(double time) {
+		if (time < 0) {
+			throw new IllegalArgumentException("Illegar argument! Time is non-negative.");
+		}
+		return (int) (0.5 * time);
+	}
 
-    @Override
-    public int liters(double time) {
-        return 0;
-    }
     /**
 	 * Bob needs a fast way to calculate the volume of a cuboid with three values:
 	 * length, width and the height of the cuboid. Write a function to help Bob with
@@ -30,28 +44,6 @@ public class EightImpl implements Eight {
 		return length * width * height;
 	}
 
-	/**
-	 * Nathan loves cycling. Because Nathan knows it is important to stay hydrated,
-	 * he drinks 0.5 litres of water per hour of cycling. You get given the time in
-	 * hours and you need to return the number of litres Nathan will drink, rounded
-	 * to the smallest value.
-	 * 
-	 * @param time - A time
-	 * @exception IllegalArgumentException IF (time < 0)
-	 * @return the number of litres rounded to the smallest value.
-	 */
-	@Override
-	public int liters(double time) {
-		if (time < 0) {
-			throw new IllegalArgumentException("Illegar argument! Time is non-negative.");
-		}
-		return (int) (0.5 * time);
-	}
-   
-    @Override
-    public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
-    }
 
     @Override
     public float mpgToKPM(float mpg) {
