f641a249b17e49da4cae8a405daafe470c1e7c77
vladdmytriv <vladdmutriv@gmail.com>
2021-09-15 20:53:48 +0300

    whereIsHe

--- src/main/java/com/ss/ita/kata/implementation/vladdmytriv/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vladdmytriv/SevenImpl.java
@@ -2,6 +2,8 @@ package com.ss.ita.kata.implementation.vladdmytriv;
 
 import com.ss.ita.kata.Seven;
 
+import java.util.ArrayList;
+
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
@@ -10,11 +12,22 @@ public class SevenImpl implements Seven {
 
     @Override
     public String seriesSum(int n) {
+
         return null;
     }
 
     @Override
-    public int whereIsHe(int p, int bef, int aft) {
-        return 0;
+    public ArrayList<Integer> whereIsHe(int p, int bef, int aft) {
+        ArrayList<Integer> positions = new ArrayList<>();
+        int a = 0;
+        int b = 0;
+        for (int i = 1; i <= p; i++) {
+            a=i-1;
+            b=p-i;
+            if (a >=bef && b <=aft){
+                positions.add(i);
+            }
+        }
+        return positions;
     }
 }
