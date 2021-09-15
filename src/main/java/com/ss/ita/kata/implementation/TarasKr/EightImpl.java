3fc26d218879af0dc2825f6be169eaac60ceadf2
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-09-15 13:38:25 +0300

    edit #3

--- src/main/java/com/ss/ita/kata/implementation/TarasKr/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/TarasKr/EightImpl.java
@@ -7,7 +7,6 @@ public class EightImpl implements Eight {
     public int liters(double time) {
         return 0;
     }
-
     /**
 	 * Bob needs a fast way to calculate the volume of a cuboid with three values:
 	 * length, width and the height of the cuboid. Write a function to help Bob with
