c7ccd1a6d372c7add68b356634f62f79165836dc
vladdmytriv <vladdmutriv@gmail.com>
2021-09-16 16:33:30 +0300

    good

--- src/main/java/com/ss/ita/kata/implementation/vladdmytriv/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vladdmytriv/EightImpl.java
@@ -36,16 +36,25 @@ public class EightImpl implements Eight {
     @Override
     public boolean amIWilson(double n) {
         double i = 1;
+        int temp;
+        boolean isPrime = true;
+        for (int k=2;k<=n/2;k++){
+            temp = (int) (n%k);
+            if (temp == 0) {
+                isPrime=false;
+                break;
+            }
+        }
         for (int j = 1; j < n; j++) {
             i = i * j;
         }
-        if (((i + 1) / n * n)%1==0) {
+        if (((i + 1) / n * n) % 1 == 0 && isPrime) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
 
     @Override
     public double twoDecimalPlaces(double number) {
