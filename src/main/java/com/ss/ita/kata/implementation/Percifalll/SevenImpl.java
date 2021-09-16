c89cb8825acdd8738799f7d7df2221c3a4cda984
percifalll <genero.frido@gmail.com>
2021-09-16 20:02:04 +0300

    Kostyra/sum-of-first-nth-series

--- src/main/java/com/ss/ita/kata/implementation/Percifalll/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/Percifalll/SevenImpl.java
@@ -8,9 +8,26 @@ public class SevenImpl implements Seven {
         return 0;
     }
 
+    /**
+     * Your task is to write a function which returns the sum of following series upto nth term(parameter).
+     * Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+     * Rules:
+     *     You need to round the answer to 2 decimal places and return it as String.
+     *     If the given value is 0 then it should return 0.00
+     *     You will only be given Natural Numbers as arguments.
+     */
     @Override
     public String seriesSum(int n) {
-        return null;
+        if (n == 0) return "0.00";
+        double series = 1;
+        double sum = 0;
+
+        for (int i = n; i > 0; i--) {
+            sum += 1 / series;
+            series += 3;
+        }
+
+        return String.format("%.2f", sum);
     }
 
     @Override
