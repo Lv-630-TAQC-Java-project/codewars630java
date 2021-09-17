c05af881aca9c6e2fe5ea99c698e614ebe1e67e3
percifalll <genero.frido@gmail.com>
2021-09-17 13:55:21 +0300

    Task completed. Not that easy balance :)

--- src/main/java/com/ss/ita/kata/implementation/Percifalll/SixImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/Percifalll/SixImpl.java
@@ -1,5 +1,7 @@
 package com.ss.ita.kata.implementation.Percifalll;
 
+import java.util.Arrays;
+
 public class SixImpl implements com.ss.ita.kata.Six {
     @Override
     public long findNb(long m) {
@@ -8,7 +10,43 @@ public class SixImpl implements com.ss.ita.kata.Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        if (book == null || book.length() == 0) throw new IllegalArgumentException();
+
+        // Cleaning
+        String[] lines = book.split("\n");
+        String[] cleanedLines = Arrays.stream(lines)
+                .map(s -> s.replaceAll("[^a-zA-Z0-9 .]*", ""))
+                .map(s -> s.replaceAll("( {2})+", " "))
+                .map(s -> s.replaceAll("\\s*$", ""))
+                .toArray(String[]::new);
+        cleanedLines[0] = cleanedLines[0].replaceAll("\\s*", "");
+
+        // Getting expenses array from cleaned lines
+        double[] expenses = Arrays.stream(
+                // Those are info lines with prices and more
+                Arrays.copyOfRange(cleanedLines, 1, cleanedLines.length))
+                .map(s -> s.split("\\s"))
+                .map(array -> array[array.length - 1])
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+
+        double balance = Double.parseDouble(cleanedLines[0]);
+        double sum = Arrays.stream(expenses).sum();
+        double avg = Math.round(sum / expenses.length * 100.0) / 100.0;
+
+        cleanedLines[0] = String.format("\n\rOriginal Balance: %.2f", balance);
+
+        double currentBalance = balance;
+        for (int i = 1; i < cleanedLines.length; i++) {
+            currentBalance -= expenses[i - 1];
+            cleanedLines[i] += String.format(" Balance %.2f", currentBalance);
+        }
+
+        String[] result = Arrays.copyOf(cleanedLines, cleanedLines.length + 2);
+        result[result.length - 2] = String.format("Total expense  %.2f", sum);
+        result[result.length - 1] = String.format("Average_expense  %.2f", avg);
+
+        return String.join("\n\r", result);
     }
 
     @Override
