8c4521a6d9ca997bc56844da43de2b6e703c5b58
vladdmytriv <vladdmutriv@gmail.com>
2021-09-17 16:32:18 +0300

    Balance

--- src/main/java/com/ss/ita/kata/implementation/vladdmytriv/Six.java
+++ src/main/java/com/ss/ita/kata/implementation/vladdmytriv/Six.java
@@ -1,5 +1,8 @@
 package com.ss.ita.kata.implementation.vladdmytriv;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.String.*;
+
 public class Six implements com.ss.ita.kata.Six {
     @Override
     public long findNb(long m) {
@@ -8,7 +11,24 @@ public class Six implements com.ss.ita.kata.Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        String[] bookMass = book
+                .replaceAll("[^a-zA-Z0-9 .\n]", "")
+                //.replaceAll("[-+!?/@#$%^&*(){},'`~Ё:<>=;]", "")
+                .split("\n");
+        int numberOfPurchase = bookMass.length - 1;
+
+        double expense = 0;
+        double originalBalance = parseDouble(bookMass[0]);
+        bookMass[0]="Original Balance: " + originalBalance;
+        for (int i = 1; i < bookMass.length; i++) {
+            String[] oneLine = bookMass[i].split(" ");
+            double price = parseDouble(oneLine[2]);
+            expense += price;
+            String newBalance = valueOf(originalBalance - price);
+            bookMass[i] = join(" ", oneLine).concat(" Balance " + newBalance);
+        }
+        String finalLine = join("\n", bookMass).concat("\nTotal expense " + format("%.2f",expense) + "\nAverage expense " + format("%.2f",expense / numberOfPurchase)).replaceAll(",",".");
+        return finalLine;
     }
 
     @Override
