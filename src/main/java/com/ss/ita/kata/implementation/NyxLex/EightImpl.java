d57404cb5358c423fde39b66e8c0d4ad2f95a5b7
Yurka Kurian <55287736+NyxLex@users.noreply.github.com>
2021-09-17 10:58:45 +0300

    Update EightImpl.java

--- src/main/java/com/ss/ita/kata/implementation/NyxLex/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/NyxLex/EightImpl.java
@@ -20,7 +20,13 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+       for (int i = 0; i < array.length; i++) {
+            if (Math.sqrt(array[i])*Math.sqrt(array[i])==array[i]){
+                array[i]= (int) Math.sqrt(array[i]);
+            }
+            else array[i] = array[i]*array[i];
+        }
+        return array;
     }
 
     @Override
