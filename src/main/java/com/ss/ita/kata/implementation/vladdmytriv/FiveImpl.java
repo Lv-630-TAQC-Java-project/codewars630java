4f19aaaa22d7243bfa5f667d389be4f7496eeb0d
vladdmytriv <vladdmutriv@gmail.com>
2021-09-19 23:27:24 +0300

    artificialRain

--- src/main/java/com/ss/ita/kata/implementation/vladdmytriv/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vladdmytriv/FiveImpl.java
@@ -3,11 +3,46 @@ package com.ss.ita.kata.implementation.vladdmytriv;
 import com.ss.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FiveImpl implements Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        ArrayList<Integer> massOfNum = new ArrayList<Integer>();
+        int count = 1;
+        int vSize = v.length;
+
+        if (vSize == 1)
+            return 1;
+
+        for (int i = 0; i < vSize; i++) {
+            for (int j = i; j < vSize; j++) {
+                if (i != vSize - 1 && j != vSize - 1 && v[j] >= v[j + 1]) {
+                    count++;
+                }
+                if (j == vSize - 1 && v[j] < v[j - 1]) {
+                    count++;
+                }
+                if (j != vSize - 1 && v[j] < v[j + 1])
+                    break;
+            }
+
+            for (int k = i; k >= 0; k--) {
+                if (k != 0 && v[k] >= v[k - 1]) {
+                    count++;
+                }
+                if (k == 0 && v[k] < v[k + 1]) {
+                    count++;
+                }
+                if (k != 0 && v[k] < v[k - 1])
+                    break;
+            }
+            massOfNum.add(count);
+            count = 0;
+        }
+
+        return Collections.max(massOfNum);
     }
 
     private static boolean prime(long i) {
@@ -25,7 +60,7 @@ public class FiveImpl implements Five {
         for (long i = m; i <= n; i++) {
             if (prime(i)) {
                 if (i - x == g) {
-                    return new long[] { x, i };
+                    return new long[]{x, i};
                 }
                 x = i;
             }
