183e6368e2855c333c328bd467aea6bb79182542
Dmytro Zhyvko <d.zhyvko@gmail.com>
2021-09-19 17:27:49 +0300

    Implemented Number of trailing zeros on N!

--- src/main/java/com/ss/ita/kata/implementation/dzhyv/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/dzhyv/FiveImpl.java
@@ -5,33 +5,37 @@ import com.ss.ita.kata.Five;
 import java.math.BigInteger;
 
 public class FiveImpl implements Five {
-    @Override
-    public int artificialRain(int[] v) {
-        return 0;
-    }
+	@Override
+	public int artificialRain(int[] v) {
+		return 0;
+	}
 
-    @Override
-    public long[] gap(int g, long m, long n) {
-        return new long[0];
-    }
+	@Override
+	public long[] gap(int g, long m, long n) {
+		return new long[0];
+	}
 
-    @Override
-    public int zeros(int n) {
-        return 0;
-    }
+	@Override
+	public int zeros(int n) {
+		int count = 0;
+		for (int i = 5; n / i >= 1; i = i * 5) {
+			count = count + n / i;
+		}
+		return count;
+	}
 
-    @Override
-    public BigInteger perimeter(BigInteger n) {
-        return null;
-    }
+	@Override
+	public BigInteger perimeter(BigInteger n) {
+		return null;
+	}
 
-    @Override
-    public double solveSum(double m) {
-        return 0;
-    }
+	@Override
+	public double solveSum(double m) {
+		return 0;
+	}
 
-    @Override
-    public long[] smallest(long n) {
-        return new long[0];
-    }
+	@Override
+	public long[] smallest(long n) {
+		return new long[0];
+	}
 }
