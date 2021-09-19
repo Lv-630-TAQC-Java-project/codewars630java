308449cce189c1f5f17d524bb69a49b4a4c803d2
percifalll <genero.frido@gmail.com>
2021-09-19 13:23:49 +0300

    Kostyra/prime-gap

--- src/main/java/com/ss/ita/kata/implementation/Percifalll/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/Percifalll/FiveImpl.java
@@ -3,6 +3,9 @@ package com.ss.ita.kata.implementation.Percifalll;
 import com.ss.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.LongStream;
 
 public class FiveImpl implements Five {
     @Override
@@ -12,7 +15,22 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        List<Long> tempPrimes = new ArrayList<>();
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) tempPrimes.add(i);
+        }
+        long[] primes = tempPrimes.stream().mapToLong(a->a).toArray();
+        if (primes.length == 0) return null;
+        for(int i = 0; i < primes.length - 1; i++){
+            if (Math.abs(primes[i] - primes[i + 1]) == g) return new long[]{primes[i], primes[i+1]};
+        }
+        return null;
+    }
+
+    private static boolean isPrime(long number) {
+        return number > 1
+                && LongStream.rangeClosed(2, (long) Math.sqrt(number))
+                .noneMatch(n -> (number % n == 0));
     }
 
     @Override
