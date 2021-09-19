fc5242e054a275bff519e67747a71395cbeb0b40
percifalll <genero.frido@gmail.com>
2021-09-19 14:33:35 +0300

    Kostyra/perimeter-of-squares

--- src/main/java/com/ss/ita/kata/implementation/Percifalll/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/Percifalll/FiveImpl.java
@@ -22,7 +22,24 @@ public class FiveImpl implements Five {
 
     @Override
     public BigInteger perimeter(BigInteger n) {
-        return null;
+        BigInteger n_1 = BigInteger.ONE;
+        BigInteger n_2 = BigInteger.ZERO;
+        BigInteger sum = BigInteger.ONE;
+
+        for (BigInteger i = BigInteger.valueOf(2); i.compareTo(n.add(BigInteger.ONE)) <= 0; i = i.add(BigInteger.ONE)){
+            BigInteger current = n_1.add(n_2);
+            sum = sum.add(current);
+            n_2 = n_1;
+            n_1 = current;
+        }
+
+        return sum.multiply(BigInteger.valueOf(4));
+    }
+
+    public static void main(String[] args) {
+        FiveImpl a = new FiveImpl();
+        BigInteger number = new BigInteger("5");
+        System.out.println(a.perimeter(number));
     }
 
     @Override
