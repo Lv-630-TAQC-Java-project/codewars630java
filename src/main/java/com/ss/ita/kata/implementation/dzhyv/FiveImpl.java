bf6d6728934b14e60c88650b22d1209d48c46d72
Dmytro Zhyvko <d.zhyvko@gmail.com>
2021-09-19 16:58:21 +0300

    Implemented Gap in primes method

--- src/main/java/com/ss/ita/kata/implementation/dzhyv/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/dzhyv/FiveImpl.java
@@ -5,33 +5,52 @@ import com.ss.ita.kata.Five;
 import java.math.BigInteger;
 
 public class FiveImpl implements Five {
-    @Override
-    public int artificialRain(int[] v) {
-        return 0;
-    }
+	@Override
+	public int artificialRain(int[] v) {
+		return 0;
+	}
 
-    @Override
-    public long[] gap(int g, long m, long n) {
-        return new long[0];
-    }
+	@Override
+	public long[] gap(int g, long m, long n) {
+		long a = 0;
+		boolean prime;
+		for (long i = m; i <= n; i++) {
+			if (isPrime(i)) {
+				if (i - a == g) {
+					return new long[] { a, i };
+				}
+				a = i;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public int zeros(int n) {
-        return 0;
-    }
+	private static boolean isPrime(long i) {
+		for (long j = 2; j < i / 2; j++) {
+			if (i % j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    @Override
-    public BigInteger perimeter(BigInteger n) {
-        return null;
-    }
+	@Override
+	public int zeros(int n) {
+		return 0;
+	}
 
-    @Override
-    public double solveSum(double m) {
-        return 0;
-    }
+	@Override
+	public BigInteger perimeter(BigInteger n) {
+		return null;
+	}
 
-    @Override
-    public long[] smallest(long n) {
-        return new long[0];
-    }
+	@Override
+	public double solveSum(double m) {
+		return 0;
+	}
+
+	@Override
+	public long[] smallest(long n) {
+		return new long[0];
+	}
 }
