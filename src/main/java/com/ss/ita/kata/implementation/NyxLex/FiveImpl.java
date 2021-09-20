4cc1f294d3d9896e07c3663f3cac53a5d9f72c42
Yurka Kurian <55287736+NyxLex@users.noreply.github.com>
2021-09-20 12:15:42 +0300

    Smallest

--- src/main/java/com/ss/ita/kata/implementation/NyxLex/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/NyxLex/FiveImpl.java
@@ -32,6 +32,45 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        List<Integer> indexMinList = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        long min = 10;
+        String str = Long.toString(n);
+        String[] digits = Integer.toString((int) n).split("");
+        long[] arr = new long[str.length()];
+        for (int i = 0; i < arr.length; i++) {
+           arr[i]= Long.parseLong(digits[i]);
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]<=min) {
+                min=arr[i];
+                indexMinList.add(i);
+            }
+        }
+
+        long c = arr[0];
+        arr[0]=arr[indexMinList.get(indexMinList.size()-1)];
+        arr[indexMinList.get(indexMinList.size()-1)]=c;
+
+        list.removeAll(indexMinList);
+
+        for (long l : arr) {
+            list.add((int) l);
+
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i)!=0)
+                break;
+            while (list.get(i)==0){
+                list.remove(i);
+            }
+        }
+
+        long[] res = new long[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i]=list.get(i);
+        }
+        return res;
     }
 }
