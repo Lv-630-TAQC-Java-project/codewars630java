4f5c59a63be0da3fde922a10525b52bc0d9ffdae
YevgenAleksandrovich <l.jegoush@gmail.com>
2021-09-20 11:55:29 +0300

    RomanovYevgen_Eight_Seven

--- src/main/java/com/ss/ita/kata/implementation/YevgenAleksandrovich/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/YevgenAleksandrovich/EightImpl.java
@@ -1,50 +1,183 @@
 package com.ss.ita.kata.implementation.YevgenAleksandrovich;
-
 import com.ss.ita.kata.Eight;
+import java.util.stream.*;
+import java.util.Arrays;
 
-public class EightImpl implements Eight {
-    @Override
+public class EightImpl<var> implements Eight {
+    private double time;
+
+    /**
+     * Nathan loves cycling. Because Nathan knows it is important to stay hydrated,
+     * he drinks 0.5 litres of water per hour of cycling. You get given the time in
+     * hours and you need to return the number of litres Nathan will drink, rounded
+     * to the smallest value.
+     *
+     * @param time - A time
+     * @return the number of litres rounded to the smallest value.
+     * @throws IllegalArgumentException IF (time < 0)
+     */
     public int liters(double time) {
-        return 0;
+        this.time = time;
+        if (time < 0) {
+            throw new IllegalArgumentException();
+        }
+        return (int) (0.5 * time);
     }
 
     @Override
-    public double getVolumeOfCuboid(double length, double width, double height) {
+    public <time> int liters() {
         return 0;
     }
+    /**
+        Bob needs a fast way to calculate the volume of a cuboid with three values:
+        length, width and the height of the cuboid.
+        Write a function to help Bob with this calculation.
 
+    */
+
+
+    @Override
+
+    public double getVolumeOfCuboid(double length, double width, double height) {
+
+        if (length < 0 || width < 0 || height < 0) {
+           return 0;
+        }
+        return length * width * height;
+
+
+    }
+/**
+Sometimes, I want to quickly be able to convert miles per imperial gallon into kilometers per liter.
+
+Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+Make sure to round off the result to two decimal points. If the answer ends with a 0, it should be rounded off without the 0. So instead of 5.50, we should get 5.5.
+
+Some useful associations relevant to this kata: 1 Imperial Gallon = 4.54609188 litres 1 Mile = 1.609344 kilometres
+*/
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        final float kmInMiles = 1.609344f;
+        final float lInGallon = 4.54609188f;
+        final float coefficient = kmInMiles / lInGallon;
+
+        float result = mpg * coefficient;
+        return Float.parseFloat(String.format("%.2f", result));
     }
+    /**
+     * Write a method, that will get an integer array as parameter
+     * and will process every number from this array.
+     * Return a new array with processing every number of the input-array like this:
+     * If the number has an integer square root, take this, otherwise square the number.
+     * [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+     * The input array will always contain only positive numbers and will never be empty or null.
+     * The input array should not be modified!
+     */
+
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+
+        if (array == null) throw new NullPointerException();
+        if (array.length == 0) throw new IllegalArgumentException();
+
+        int[] result = new int[array.length];
+
+        for (int i = 0; i < result.length; i++) {
+            double root = Math.sqrt(array[i]);
+
+            if (Math.floor(root) == root) {
+                result[i] = (int) root;
+            } else {
+                result[i] = array[i] * array[i];
+            }
+        }
+
+        return result;
+
     }
+
+/**Given an array of integers.
+
+ Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+ If the input array is empty or null, return an empty array.
+
+ Example
+ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if (input == null || input.length == 0) {
+            int[] emptyArray = {};
+            return emptyArray;
+        }
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] > 0) {
+                count++;
+            } else if (input[i] < 0) {
+                sum += input[i];
+            }
+        }
+        int[] returnValues = new int[] {count, sum};
+        return returnValues;
     }
+/**Description
+ We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+ Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+ Examples
+ "1234" --> 1234
+ "605"  --> 605
+ "1405" --> 1405
+ "-7" --> -7*/
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
     }
 
+    /**Wilson primes satisfy the following condition. Let P represent a prime number.
+
+     Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+     Your task is to create a function that returns true if the given number is a Wilson prime.*/
     @Override
     public boolean amIWilson(double n) {
-        return false;
-    }
+        for (int j = 2; j < n; j++) {
+            if (n % j == 0) {
+                return false;
+            }
+        }
+        return true;
 
+    }
+/**Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+ Example:
+ 5.5589 is rounded 5.56
+ 3.3424 is rounded 3.34*/
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        return Double.parseDouble(String.format("%.2f", number));
     }
+
+    /**Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+     Example
+     divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]*/
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+
+        return Arrays.stream(numbers)
+                .filter(i -> (i % divider) == 0)
+                .toArray();
+
     }
 }
--- src/main/java/com/ss/ita/kata/implementation/YevgenAleksandrovich/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/YevgenAleksandrovich/SevenImpl.java
@@ -1,20 +1,115 @@
 package com.ss.ita.kata.implementation.YevgenAleksandrovich;
-
 import com.ss.ita.kata.Seven;
 
-public class SevenImpl implements Seven {
+public abstract class SevenImpl<function> implements Seven {
+
+    /**
+     * The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he made a mistake. Could you help him?
+     * <p>
+     * if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
+     * <p>
+     * The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
+     * <p>
+     * Should the last donation be a non positive number (<= 0) John wants us to throw (or raise) an error or
+     * <p>
+     * return Nothing in Haskell
+     * return None in F#, Ocaml, Scala
+     * return -1 in C, Fortran, Nim, PowerShell, Go, Pascal, Prolog, Lua, Perl
+     * echo ERROR in Shell
+     * raise-argument-error in Racket
+     * so that he clearly sees that his expectations are not great enough.
+     * <p>
+     * Notes:
+     * <p>
+     * all donations and navg are numbers (integers or floats), arr can be empty.
+     * See examples below and "Test Samples" to see which return is to be done.
+     * new_avg([14, 30, 5, 7, 9, 11, 15], 92) should return 645
+     * new_avg([14, 30, 5, 7, 9, 11, 15], 2)
+     * should raise an error (ValueError or invalid_argument or argument-error or DomainError)
+     * or return `-1` or ERROR or Nothing or None depending on the language.
+     */
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        //navg
+        //arr.length
+        //find sum
+        double sum = 0;
+        for (double number : arr) {
+            sum += number;
+        }
+        long res = (long) Math.ceil(navg * (arr.length + 1) - sum);
+        if (res <= 0)
+            throw new IllegalArgumentException();
+        else
+            return res;
     }
+/**Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+ Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+ Rules:
+ You need to round the answer to 2 decimal places and return it as String.
+
+ If the given value is 0 then it should return 0.00
+
+ You will only be given Natural Numbers as arguments.
+
+ Examples:
+ SeriesSum(1) => 1 = "1.00"
+ SeriesSum(2) => 1 + 1/4 = "1.25"
+ SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+ * @return*/
 
     @Override
-    public String seriesSum(int n) {
-        return null;
-    }
+    public int seriesSum(int n) {
+        function SeriesSum;
+        Object let;
+        double result;
+         {
+                let  = 0;
+        int reverage = 4;
+        for (int i = 0; i < n; i += 1) {
+            if (i == 0) {
+                result = 1;
+            } else {
+                reverage += 3;
 
-    @Override
-    public int whereIsHe(int p, int bef, int aft) {
+            }
+        }
         return 0;
+}
     }
 }
+
+    /**Vasya stands in line with number of people p (including Vasya), but he doesn't know exactly which position he occupies. He can say that there are no less than b people standing in front of him and no more than a people standing behind him. Find the number of different positions Vasya can occupy.
+
+     Input
+     As an input you have 3 numbers:
+
+     1. Total amount of people in the line;
+
+     2. Number of people standing in front of him
+
+     3. Number of people standing behind him
+
+     Examples:
+     WhereIsVasya.whereIsHe(3, 1, 1)   // => 2  The possible positions are: 2 and 3
+
+     WhereIsVasya.whereIsHe(5, 2, 3)  // => 3  The possible positions are: 3, 4 and 5
+
+    @Override
+    public void whereIsHe(int p, int bef, int aft) {
+        return 0;
+
+            ArrayList<Integer> positions = new ArrayList<>();
+            int a = 0;
+            int b = 0;
+            for (int i = 1; i <= p; i++) {
+                a=i-1;
+                b=p-i;
+                if (a >=bef && b <=aft){
+                    positions.add(i);
+                }
+            }
+            return ;
+        }
+
