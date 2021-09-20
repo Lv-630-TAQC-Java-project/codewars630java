6e6c9f7d85c0bdfb5ec69a6d8caf7c1441addcec
Yurka Kurian <55287736+NyxLex@users.noreply.github.com>
2021-09-20 12:08:52 +0300

    Gap in Primes

--- src/main/java/com/ss/ita/kata/implementation/NyxLex/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/NyxLex/FiveImpl.java
@@ -11,8 +11,32 @@ public class FiveImpl implements Five {
     }
 
     @Override
-    public long[] gap(int g, long m, long n) {
-        return new long[0];
+   public long[] gap(int g, long m, long n) {
+        long[] rs = new long[2];
+        List<Integer> list = new LinkedList<>() ;
+        for(int i = (int) m; i<=n; i++)
+            if(isPrime(i)){
+                list.add(i);
+            }
+        for (int i = 0; i < list.size()-1; i++) {
+            if (list.get(i+1)-list.get(i)==g){
+                rs[0]= Long.parseLong(list.get(i).toString());
+                rs[1]= Long.parseLong(list.get(i+1).toString());
+                return new long[] { rs[0], rs[1] };
+            }
+        }
+        System.out.println(Arrays.toString(rs));
+        return null;
+    }
+
+    static boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
     }
 
     @Override
