349f87aebaa18b97b3720bc6d249d8a31d7e4b1b
Yura <yurkakuryan@gmail.com>
2021-09-22 21:03:16 +0300

    skelet

--- src/main/java/com/ss/ita/utils/Runner.java
+++ src/main/java/com/ss/ita/utils/Runner.java
@@ -1,5 +1,7 @@
 package com.ss.ita.utils;
 
+
+import com.ss.ita.kata.Eight;
 import com.ss.ita.kata.implementation.users.User;
 
 public class Runner {
@@ -9,4 +11,32 @@ public class Runner {
     public void setUser(User user){
         this.user = user;
     }
+
+    public void TASK_8_1() {}
+    public void TASK_8_2() {}
+    public void TASK_8_3() {}
+    public void TASK_8_4() {}
+    public void TASK_8_5() {}
+    public void TASK_8_6() {}
+    public void TASK_8_7() {}
+    public void TASK_8_8() {}
+    public void TASK_8_9() {}
+
+    public void TASK_7_1() {}
+    public void TASK_7_2() {}
+    public void TASK_7_3() {}
+
+    public void TASK_6_1() {}
+    public void TASK_6_2() {}
+    public void TASK_6_3() {}
+    public void TASK_6_4() {}
+    public void TASK_6_5() {}
+    public void TASK_6_6() {}
+
+    public void TASK_5_1() {}
+    public void TASK_5_2() {}
+    public void TASK_5_3() {}
+    public void TASK_5_4() {}
+    public void TASK_5_5() {}
+    public void TASK_5_6() {}
 }
