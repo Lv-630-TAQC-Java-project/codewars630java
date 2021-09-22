8813fe06b91c0b9cc253cdb47a11bb1561211764
percifalll <genero.frido@gmail.com>
2021-09-22 20:34:59 +0300

    Runner update

--- src/main/java/com/ss/ita/utils/Runner.java
+++ src/main/java/com/ss/ita/utils/Runner.java
@@ -1,12 +1,21 @@
 package com.ss.ita.utils;
 
+import com.ss.ita.kata.*;
 import com.ss.ita.kata.implementation.users.User;
 
 public class Runner {
     private ConsoleScanner scanner = new ConsoleScanner();
-    private User user;
+    private Eight eight;
+    private Seven seven;
+    private Six six;
+    private Five five;
 
     public void setUser(User user){
-        this.user = user;
+        Holder holder = user.getHolder();
+
+        eight = holder.getEightImpl();
+        seven = holder.getSevenImpl();
+        six = holder.getSixImpl();
+        five = holder.getFiveImpl();
     }
 }
