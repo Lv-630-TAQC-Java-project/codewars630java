ce0c3bc194f169389140aee8f5a7715f8d80d958
anastasia8755 <anastasia.sydor331@gmail.com>
2021-09-24 15:14:23 +0300

    Closes #270

--- src/main/java/com/ss/ita/kata/implementation/anastasia8755/SixImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/anastasia8755/SixImpl.java
@@ -3,7 +3,19 @@ package com.ss.ita.kata.implementation.anastasia8755;
 public class SixImpl implements com.ss.ita.kata.Six {
     @Override
     public long findNb(long m) {
-        return 0;
+        for (int cubeCount = 0; ; cubeCount++) {
+            //n^3 + (n-1)^3 + ... + 1^3 = m if m=0 => cubeCount=0
+            if (m > 0) {
+                long currCubeVol = (long) Math.pow(cubeCount + 1, 3);
+                m = (long) m - currCubeVol;
+
+            } else if (m == 0) {
+                return cubeCount;
+
+            } else if (m < 0) {
+                return (-1);
+            }
+        }
     }
 
     @Override
