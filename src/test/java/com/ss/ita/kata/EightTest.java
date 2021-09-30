346ca29753ca91f98f8f4f82095a07cd81445090
Liubomyr Halamaha <galamagal86@gmail.com>
2021-09-30 13:42:23 +0300

    fix

--- src/test/java/com/ss/ita/kata/EightTest.java
+++ src/test/java/com/ss/ita/kata/EightTest.java
@@ -1,35 +1,33 @@
 package com.ss.ita.kata;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
 
 public class EightTest extends EightDataProvider {
 
     @Test(dataProvider = "dpTpMPG")
     public void testMpgToKPM(Eight impl, float mpg, float expected) {
-        assertEquals(impl.mpgToKPM(mpg), expected);
+        Assert.assertEquals(impl.mpgToKPM(mpg), expected);
     }
 
     @Test(dataProvider = "dpAmIWilson")
     public void testAmIWilson(Eight impl, double number, boolean expected) {
-        assertEquals(impl.amIWilson(number), expected, "Error for number \"" + number + "\";");
+        Assert.assertEquals(impl.amIWilson(number), expected, "Error for number \"" + number + "\";");
     }
 
     @Test(dataProvider = "dpCountPositivesSumNegatives")
     public void testCountPositivesSumNegatives(Eight impl, int[] input, int[] expected) {
-        assertEquals(impl.countPositivesSumNegatives(input), expected);
+        Assert.assertEquals(impl.countPositivesSumNegatives(input), expected);
     }
 
     @Test(dataProvider = "dpVolumeOfCuboid")
     public void testVolumeOfCuboid(Eight impl, double lenght, double wight, double height, double expected) {
-        assertEquals(impl.getVolumeOfCuboid(lenght, wight, height), expected,0.1);
+        Assert.assertEquals(impl.getVolumeOfCuboid(lenght, wight, height), expected, 0.1);
     }
 
     @Test(dataProvider = "dpDivisibleBy")
-    public void testDivisibleBy(Eight impl, int[] input, int number, int[]expected) {
-        assertEquals(impl.divisibleBy(input, number), expected);
+    public void testDivisibleBy(Eight impl, int[] input, int number, int[] expected) {
+        Assert.assertEquals(impl.divisibleBy(input, number), expected);
     }
 
     @Test(dataProvider = "dpLiters")
