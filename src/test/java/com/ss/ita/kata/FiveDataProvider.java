4d538437d82fee6a605c43369ad482550446045b
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-10-03 20:55:31 +0300

    testSmallest

--- src/test/java/com/ss/ita/kata/FiveDataProvider.java
+++ src/test/java/com/ss/ita/kata/FiveDataProvider.java
@@ -45,5 +45,18 @@ public class FiveDataProvider extends DataProviderImpl {
         };
         return  combine(fiveImplPackProvider(), param);
     }
+    
+    @DataProvider(name = "dpSmallest")
+    public Object[][] dpSmallest(){
+    	Object[][] params = new Object[][] {
+    		{261235, new long[] {126235, 2, 0}},
+    		{209917, new long[] {29917, 0, 1}},
+    		{285365, new long[] {238565, 3, 1}},
+    		{269045, new long[] {26945, 3, 0}},
+    		{296837, new long[] {239687, 4, 1}}
+    		
+    	};
+    	 return  combine(fiveImplPackProvider(), params);
+    }
 }
 
