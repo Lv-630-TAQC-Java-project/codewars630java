934af70f443693322700f5a09b7f0ffd3453c624
vladdmytriv <vladdmutriv@gmail.com>
2021-10-08 12:00:45 +0300

    some changes

--- src/test/java/com/ss/ita/kata/SixDataProvider.java
+++ src/test/java/com/ss/ita/kata/SixDataProvider.java
@@ -5,11 +5,11 @@ import org.testng.annotations.DataProvider;
 public class SixDataProvider extends DataProviderImpl {
 
     final String b1 = "1000.00!=\n125 Market !=:125.45\n126 Hardware =34.95\n127 Video! 7.45\n128 Book :14.32\n129 Gasoline ::16.10";
-    final String b1sol = "Original Balance: 1000.00\\n125 Market 125.45 Balance 874,55\\n126 Hardware 34.95 Balance 839,60\\n127 Video 7.45 Balance 832,15\\n128 Book 14.32 Balance 817,83\\n129 Gasoline 16.10 Balance 801,73\\nTotal expense 198,27\\nAverage expense 39,65";
+    final String b1sol = "Original Balance: 1000.00\\n125 Market 125.45 Balance 874.55\\n126 Hardware 34.95 Balance 839.60\\n127 Video 7.45 Balance 832.15\\n128 Book 14.32 Balance 817.83\\n129 Gasoline 16.10 Balance 801.73\\nTotal expense 198.27\\nAverage expense 39.65";
 
     final String b2 = "1233.00\n125 Hardware;! 24.80?\n123 Flowers 93.50;\n127 Meat 120.90\n120 Picture 34.00\n124 Gasoline 11.00\n" +
             "123 Photos;! 71.40?\n122 Picture 93.50\n132 Tyres;! 19.00,?;\n129 Stamps; 13.60\n129 Fruits{} 17.60\n129 Market;! 128.00?\n121 Gasoline;! 13.60?";
-    final String b2sol = "Original Balance: 1233.00\\n125 Hardware 24.80 Balance 1208,20\\n123 Flowers 93.50 Balance 1114,70\\n127 Meat 120.90 Balance 993,80\\n120 Picture 34.00 Balance 959,80\\n124 Gasoline 11.00 Balance 948,80\\n123 Photos 71.40 Balance 877,40\\n122 Picture 93.50 Balance 783,90\\n132 Tyres 19.00 Balance 764,90\\n129 Stamps 13.60 Balance 751,30\\n129 Fruits 17.60 Balance 733,70\\n129 Market 128.00 Balance 605,70\\n121 Gasoline 13.60 Balance 592,10\\nTotal expense 640,90\\nAverage expense 53,41";
+    final String b2sol = "Original Balance: 1233.00\\n125 Hardware 24.80 Balance 1208.20\\n123 Flowers 93.50 Balance 1114.70\\n127 Meat 120.90 Balance 993.80\\n120 Picture 34.00 Balance 959.80\\n124 Gasoline 11.00 Balance 948.80\\n123 Photos 71.40 Balance 877.40\\n122 Picture 93.50 Balance 783.90\\n132 Tyres 19.00 Balance 764.90\\n129 Stamps 13.60 Balance 751.30\\n129 Fruits 17.60 Balance 733.70\\n129 Market 128.00 Balance 605.70\\n121 Gasoline 13.60 Balance 592.10\\nTotal expense 640.90\\nAverage expense 53.41";
     // Used for rainfall tests - Kata 6
     private final String rainfallSheet =
             "Ubbin Falls:Jan 265.5,Feb 442.6,Mar 268.6,Apr 372.6,May 281,Jun 287.3,Jul 237.2,Aug 117,Sep 189.8,Oct 280.2,Nov 186.7,Dec 75.1\n" +
